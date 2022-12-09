6e97088b7a3538e488f2c815d202e8bb3fbcc321
SuperHexBot <65476506+SuperHexBot@users.noreply.github.com>
2022-12-09 17:23:02 +0800

    1
    1

--- XBOXGameBarPoc_UWP/MainPage.xaml.cs
+++ XBOXGameBarPoc_UWP/MainPage.xaml.cs
@@ -33,45 +33,65 @@ namespace XBOXGameBarPoC_UWP
                 {
                     System.Numerics.Vector2 CenterTop;
                     System.Numerics.Vector2 CenterBottom;
+                    System.Numerics.Vector2 CenterY1;
+                    System.Numerics.Vector2 CenterY2;
+                    System.Numerics.Vector2 CenterX1;
+                    System.Numerics.Vector2 CenterX2;
                     CenterTop.X = 2560 / 2.0f;
                     CenterTop.Y = 0.0f;
                     CenterBottom.X = 2560 / 2.0f;
                     CenterBottom.Y = 1440.0f;
+
+                    CenterY1.X = 2560 / 2.0f;
+                    CenterY1.Y = 1440.0f / 2.0F - 10.0F;
+                    CenterY2.X = 2560 / 2.0f;
+                    CenterY2.Y = 1440.0f / 2.0F + 10.0F;
+                    CenterX1.X = 2560 / 2.0f - 10.0F;
+                    CenterX1.Y = 1440.0f / 2.0F;
+                    CenterX2.X = 2560 / 2.0f + 10.0F;
+                    CenterX2.Y = 1440.0f / 2.0F;
                     while (true)
                     {
                         using (CanvasDrawingSession ds = canvasSwapChainPanel.SwapChain.CreateDrawingSession(Colors.Transparent))
                         {
-                            int count = 0;
-                            int spectators = 0;
-                            int allied_spectators = 0;
-                            viewAccessor.Read<int>(0, out count);
-                            viewAccessor.Read<int>(4, out spectators);
-                            viewAccessor.Read<int>(8, out allied_spectators);
-                            if (count != 0)
+                            misc_xbox misc_xbox_inst;
+                            viewAccessor.Read<misc_xbox>(0, out misc_xbox_inst);
+                            if (misc_xbox_inst.esp_draw)
                             {
-                                Box[] boxArray = new Box[count];
-                                viewAccessor.ReadArray<Box>(12, boxArray, 0, count);
-                                for (int i = 0; i < boxArray.Length; i++)
+                                if (misc_xbox_inst.count != 0)
                                 {
-                                    Color boxcolor = Colors.White;
-                                    Color shieldcolor = Colors.White;
-                                    if (boxArray[i].BoxColor == 1){ boxcolor = Colors.Red;}
-                                    else if (boxArray[i].BoxColor == 2) { boxcolor = Colors.Yellow; }
-                                    else if (boxArray[i].BoxColor == 3) { boxcolor = Colors.Black; }
-                                    if (boxArray[i].ShieldColor == 1) { shieldcolor = Colors.Blue; }
-                                    else if (boxArray[i].ShieldColor == 2) { shieldcolor = Colors.Purple; }
-                                    else if (boxArray[i].ShieldColor == 3) { shieldcolor = Colors.Red; }
+                                    Box[] boxArray = new Box[misc_xbox_inst.count];
+                                    viewAccessor.ReadArray<Box>(24, boxArray, 0, misc_xbox_inst.count);
+                                    for (int i = 0; i < boxArray.Length; i++)
+                                    {
+                                        Color boxcolor = Colors.White;
+                                        Color shieldcolor = Colors.White;
+                                        if (boxArray[i].BoxColor == 1) { boxcolor = Colors.Red; }
+                                        else if (boxArray[i].BoxColor == 2) { boxcolor = Colors.Yellow; }
+                                        else if (boxArray[i].BoxColor == 3) { boxcolor = Colors.Black; }
+                                        if (boxArray[i].ShieldColor == 1) { shieldcolor = Colors.Blue; }
+                                        else if (boxArray[i].ShieldColor == 2) { shieldcolor = Colors.Purple; }
+                                        else if (boxArray[i].ShieldColor == 3) { shieldcolor = Colors.Red; }
 
-                                    ds.DrawRectangle(boxArray[i].X, boxArray[i].Y - boxArray[i].Height, boxArray[i].Width, boxArray[i].Height, boxcolor, 1);
-                                    ds.DrawLine(boxArray[i].X - 5, boxArray[i].Y, boxArray[i].X - 5, boxArray[i].Y - boxArray[i].HpHeight, Colors.Red, 5);
-                                    ds.DrawLine(boxArray[i].X + boxArray[i].Width / 2 + 5, boxArray[i].Y, boxArray[i].X + boxArray[i].Width / 2 + 5, boxArray[i].Y - boxArray[i].ShieldHeight, shieldcolor, 5);
-                                    System.Numerics.Vector2 BoxTop;
-                                    BoxTop.X = boxArray[i].X + boxArray[i].Width / 2.0f;
-                                    BoxTop.Y = boxArray[i].Y;
-                                    ds.DrawLine(CenterBottom, BoxTop, boxcolor, 1);
+                                        ds.DrawRectangle(boxArray[i].X, boxArray[i].Y - boxArray[i].Height, boxArray[i].Width, boxArray[i].Height, boxcolor, 1);
+                                        ds.DrawLine(boxArray[i].X - 5, boxArray[i].Y, boxArray[i].X - 5, boxArray[i].Y - boxArray[i].HpHeight, Colors.Red, 5);
+                                        ds.DrawLine(boxArray[i].X + boxArray[i].Width / 2 + 5, boxArray[i].Y, boxArray[i].X + boxArray[i].Width / 2 + 5, boxArray[i].Y - boxArray[i].ShieldHeight, shieldcolor, 5);
+                                        System.Numerics.Vector2 BoxTop;
+                                        BoxTop.X = boxArray[i].X + boxArray[i].Width / 2.0f;
+                                        BoxTop.Y = boxArray[i].Y;
+                                        ds.DrawLine(CenterBottom, BoxTop, boxcolor, 1);
+                                    }
                                 }
+                                ds.DrawText(misc_xbox_inst.spectators + "-" + misc_xbox_inst.allied_spectators + "-" + misc_xbox_inst.bone + "-" + misc_xbox_inst.aim_no_recoil + "-" + misc_xbox_inst.aim_mov_x + "-" + misc_xbox_inst.aim_mov_y_down + "-" + misc_xbox_inst.aim_mov_y_up + "-" + misc_xbox_inst.auto_changge_aim, CenterTop, Colors.White);
+
+                                Color FPSCOLOR = Colors.Red;
+                                if (misc_xbox_inst.allied_spectators !=0 || misc_xbox_inst.spectators != 0)
+                                {
+                                    FPSCOLOR = Colors.Green;
+                                }
+                                ds.DrawLine(CenterY1, CenterY2, FPSCOLOR, 5);
+                                ds.DrawLine(CenterX1, CenterX2, FPSCOLOR, 5);
                             }
-                            ds.DrawText("s:" + spectators + "-" + allied_spectators, CenterTop, Colors.White);
                             canvasSwapChainPanel.SwapChain.Present();
                         }
                     }
@@ -88,6 +108,19 @@ namespace XBOXGameBarPoC_UWP
             public float ShieldHeight;
             public int BoxColor;
             public int ShieldColor;
+        }
+        struct misc_xbox
+        {
+            public int count;
+            public int spectators;
+            public int allied_spectators;
+            public int bone;
+            public bool aim_mov_x;
+            public bool aim_mov_y_down;
+            public bool aim_mov_y_up;
+            public bool auto_changge_aim;
+            public bool aim_no_recoil;
+            public bool esp_draw;
         }
     }
 }
